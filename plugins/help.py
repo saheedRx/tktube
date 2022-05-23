37072b77b10219d72a383e3a1a19c9ae98f3302f
saheedRx <89293737+saheedRx@users.noreply.github.com>
2022-05-23 09:28:36 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Just Send Youtube Url(url only).\n\nExamples👇 ✅\nhttps://youtube.com/watch?v=xxxx \nhttps://youtu.be/xxxx\n\nDon't Send like this👇 ❌\nhttps://youtube.com/xxxx<b>?feature=share</b>\n(Remove unwanted text from your URL)\n\n\n@Tamil_Kingdom"
+    helptxt = f"Just Send Youtube Url(url only).\n\nExamples👇 ✅\nhttps://youtube.com/watch?v=xxxx \nhttps://youtu.be/xxxx\n\nDon't Send like this👇 ❌\nhttps://youtu.be/xxxx<b>?feature=share</b>\n(Remove unwanted text from your URL)\n\n\n@Tamil_Kingdom"
     await message.reply_text(helptxt)
